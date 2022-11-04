6b10f499bf1bb22f91c4102b9caedbc88afc2343
Leon Letto <leon.letto@gmail.com>
2022-11-04 14:44:19 -0700

    Updating Readme and fixing "no real password"

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# createDevCa
+# createCa - Create your own CA for your environment and issue and revoke certificates as needed.
+
+## Features:
+* Create your own CA for your environment
+* Issue certificates for your environment
+* Revoke certificates for your environment
+* Create a certificate revocation list (CRL) for your environment
+
+## Requirements:
+* openssl
+* bash
+* sed
+* grep
+* awk
+* openssl.cnf
+* options.cnf
+
+## Usage:
+* Create your own CA for your environment
+```shell
+chmod +x createCa.sh
+./createCa.sh
+```
+
+* Issue certificates for your environment
+```shell
+chmod +x newCert.sh
+./newCert.sh
+```
--- createCa.sh
+++ createCa.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
-echo 'Please enter the Certificate Authority Name for the CA you are creating.\n'
+echo 'Please enter the Certificate Authority Name for the CA you are creating.'
 echo 'This will become the filename of your CA certificates'
-read caname
+read -r caname
 #caname=myca3
-echo 'Please Answer All of the questions in as much detail as you like.\n'
-echo 'The Answers will be shown in the certs you create.\n'
+echo 'Please Answer All of the questions in as much detail as you like.'
+echo 'The Answers will be shown in the certs you create.'
 
 echo 'You are about to be asked to enter information that will be incorporated'
 echo 'into your certificate request.'
@@ -14,13 +18,13 @@ echo 'There are quite a few fields but you can leave some blank'
 echo 'For some fields there will be a default value,'
 echo 'If you enter '.', the field will be left blank.'
 echo '-----'
-echo 'Country Name (2 letter code):'; read C
-echo 'State or Province Name (full name):';read ST
-echo 'Locality Name (eg, city) :';read L
-echo 'Organization Name (eg, company) :'; read O
-echo 'Organizational Unit Name (eg, section) :'; read OU
-echo 'Common Name (eg, fully qualified host name) :'; read CN
-echo 'Email Address :'; read emailAddress
+echo 'Country Name (2 letter code):'; read -r C
+echo 'State or Province Name (full name):';read -r ST
+echo 'Locality Name (eg, city) :';read -r L
+echo 'Organization Name (eg, company) :'; read -r O
+echo 'Organizational Unit Name (eg, section) :'; read -r OU
+echo 'Common Name (eg, fully qualified host name) :'; read -r CN
+echo 'Email Address :'; read -r emailAddress
 #C=US
 #ST=CA
 #L=San
@@ -29,7 +33,67 @@ echo 'Email Address :'; read emailAddress
 #CN=myca3.com
 #emailAddress=leon@myca3.com
 
-password=password
+declare password
+VALID=true
+validCharacters='[\~\!\@\#\$\%\^\&\*\(\)\_\+]'
+echo "A password is required to protect your CA Private Key."
+echo "A valid password must:"
+echo "Have a minimum of 10 characters and a maximum of 25."
+echo "Contain at least one digits."
+echo "Contain at least one uppercase letters."
+echo "Contain at least one lowercase letters."
+echo "Contain at least one top-row character"
+echo "eg. one of: ${validCharacters//\\/}"
+echo "Not contain spaces."
+
+for (( ;; )); do
+	read -r -s -p "Please enter the password for your CA private key: " PASS1
+	echo
+	read -r -s -p "Please re-enter the password for your CA private key: " PASS2
+	echo
+	echo
+
+	if [[ "$PASS1" != "$PASS2" ]]; then
+		echo "Passwords do not match. Please try again."
+		VALID=false
+    fi
+	if L=${#PASS1}; [[ L -lt 10 || L -gt 25 ]]; then
+		echo "Password must have a minimum of 10 characters and a maximum of 25."
+		VALID=false
+    fi
+#	elif [[ $PASS1 != *[[:digit:]]*[[:digit:]]* ]]; then
+	if [[ $PASS1 != *[[:digit:]]* ]]; then
+		echo "Password should contain at least one digits."
+		VALID=false
+    fi
+#	elif [[ $PASS1 != *[[:upper:]]*[[:upper:]]* ]]; then
+	if [[ $PASS1 != *[[:upper:]]* ]]; then
+		echo "Password should contain at least one uppercase letters."
+		VALID=false
+    fi
+#	elif [[ $PASS1 != *[[:lower:]]*[[:lower:]]* ]]; then
+	if [[ $PASS1 != *[[:lower:]]* ]]; then
+		echo "Password should contain at least one lowercase letters."
+		VALID=false
+    fi
+#	elif [[ $PASS1 != *[[:punct:]]*[[:punct:]]* ]]; then
+	if ! grep -q -e "$validCharacters" <<< "$PASS1"; then
+		echo "Password should contain at least one top-row special character."
+		echo "eg. one of: ${validCharacters//\\/}"
+		VALID=false
+    fi
+	if [[ $PASS1 == *[[:blank:]]* ]]; then
+		echo "Password cannot contain spaces."
+		VALID=false
+    fi
+	if [[ "$VALID" == "true" ]]; then
+		# valid password; break out of the loop
+		echo "Password accepted."
+		password=$PASS1
+		break
+	fi
+	echo
+done
 
 mkdir ./cacerts
 mkdir ./certs
@@ -50,13 +114,13 @@ sed -i .bak "s/yourorgunitname/$OU/g" openssl.cnf
 sed -i .bak "s/yourcommonname/$CN/g" openssl.cnf
 sed -i .bak "s/youremailaddress/$emailAddress/g" openssl.cnf
 
-openssl genrsa -aes256 -passout pass:$password -out  cacerts/$caname.key 4096
-openssl req -newkey rsa:2048 -sha256 -x509 -days 1826 -key cacerts/$caname.key -out cacerts/$caname.crt -passin pass:password  -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=$CN/emailAddress=$emailAddress" -config openssl.cnf -extensions v3_ca
+openssl genrsa -aes256 -passout pass:"$password" -out  cacerts/"$caname".key 4096
+openssl req -newkey rsa:2048 -sha256 -x509 -days 1826 -key cacerts/"$caname."key -out cacerts/"$caname".crt -passin pass:"$password"  -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=$CN/emailAddress=$emailAddress" -config openssl.cnf -extensions v3_ca
 echo "1000" > serial
 echo "1000" > crl/crlnumber
 touch index.txt
 echo "unique_subject = yes" > index.txt.attr
-openssl ca -config openssl.cnf -passin pass:password -gencrl -out crl/$caname.crl.pem
+openssl ca -config openssl.cnf -passin pass:"$password" -gencrl -out crl/"$caname".crl.pem
 
 ./newCert.sh localhost
 
